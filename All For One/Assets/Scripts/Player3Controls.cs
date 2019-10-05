76c43d1b87e41ffa43f6f42613c5a10a542a77b7
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-10-05 16:32:16 -0500

    Updated controls, p3 controls script

--- All For One/Assets/Scripts/Player3Controls.cs
+++ All For One/Assets/Scripts/Player3Controls.cs
@@ -8,22 +8,53 @@ public class Player3Controls : MonoBehaviour
     string[] controllerNames;
 
     const float dead = 0.05f;
-    const float speed = 1f;
+    float speed = 0.1f;
 
     [SerializeField] float horizontal;
     [SerializeField] float vertical;
+    [SerializeField] float sprint;
+
+    Rigidbody2D thisRigidbody;
+    Vector3 position;
+
+    [SerializeField] bool attack;
+
+    bool isPlayer;
+
+    bool gotKey;
 
     // Start is called before the first frame update
     void Start()
     {
+        thisRigidbody = GetComponent<Rigidbody2D>();
 
+        if (name == "P1" || name == "P2" || name == "P3")
+        {
+            isPlayer = true;
+        }
+        else
+        {
+            isPlayer = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        position = thisRigidbody.position;
+
         horizontal = Input.GetAxisRaw("P3 Horizontal");
         vertical = Input.GetAxisRaw("P3 Vertical");
+        sprint = Input.GetAxisRaw("P3 Sprint");
+
+        if (Mathf.Abs(sprint) > dead)
+        {
+            speed = 0.2f;
+        }
+        else
+        {
+            speed = 0.1f;
+        }
 
         if (Mathf.Abs(horizontal) > dead)
         {
@@ -32,6 +63,39 @@ public class Player3Controls : MonoBehaviour
         if (Mathf.Abs(vertical) > dead)
         {
             transform.Translate(Vector3.up * speed * vertical);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Wall")
+            return;
+
+        if (collision.collider.name == "Key")
+        {
+            Destroy(collision.collider.gameObject);
+            gotKey = true;
+        }
+
+        Debug.Log(name + " " + collision.collider.name);
+
+        attack = Input.GetButtonDown("P3 Attack");
+        if (attack)
+        {
+            Debug.Log("P1 Attack");
+            HealthController otherHealthP1 = collision.collider.GetComponent<HealthController>();
+            if (otherHealthP1 != null)
+            {
+                otherHealthP1.TakeHit();
+            }
+        }
+    }
+
+    public bool GotKey
+    {
+        get
+        {
+            return gotKey;
         }
     }
 }
