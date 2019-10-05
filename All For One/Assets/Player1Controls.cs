0dc778929b3be927705996091d5db3eaf25f3487
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-10-04 20:45:07 -0500

    Updated P1 Controls

--- All For One/Assets/Player1Controls.cs
+++ All For One/Assets/Player1Controls.cs
@@ -7,10 +7,13 @@ public class Player1Controls : MonoBehaviour
     string[] controllerNames;
 
     const float dead = 0.05f;
-    const float speed = 1f;
+    const float speed = 0.5f;
 
     [SerializeField] float horizontal;
     [SerializeField] float vertical;
+
+    Rigidbody2D thisRigidbody;
+    Vector3 position;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +24,15 @@ public class Player1Controls : MonoBehaviour
         {
             Debug.Log(name);
         }
+
+        thisRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        position = thisRigidbody.position;
+
         horizontal = Input.GetAxisRaw("P1 Horizontal");
         vertical = Input.GetAxisRaw("P1 Vertical");
 
